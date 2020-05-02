42c44b5d42a3f9854d4e2675024269bbb3c38480
Kemi Thomas <kemothom71@gmail.com>
2020-05-01 22:10:11 -0400

    Updates

---
+++ precache-manifest.2b46444a159eb2633eb7c601d18a32ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28478313473ad41ad0211a0904522a8",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "ce8a376227f03189a090",
+    "url": "/react-calculator/static/css/main.c122c390.chunk.css"
+  },
+  {
+    "revision": "01c49ee8fc6ab51d042d",
+    "url": "/react-calculator/static/js/2.c6b4ff10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.c6b4ff10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8a376227f03189a090",
+    "url": "/react-calculator/static/js/main.f7e51063.chunk.js"
+  },
+  {
+    "revision": "7a4e8bb32f6cfb799366",
+    "url": "/react-calculator/static/js/runtime-main.6252dc75.js"
+  }
+]);
